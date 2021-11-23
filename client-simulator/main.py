d621a190adb8b35fcf14e249e2ee9eb32d6bd492
danjones1618 <hello@danjones.dev>
2021-11-23 19:28:43 +0000

    Send random FENs to test throughput
    LFS updates

--- client-simulator/main.py
+++ client-simulator/main.py
@@ -12,21 +12,14 @@ import typer
 class AppOptions:
     """ Represents the global options in the client """
     url: str
-    pgn: Optional[TextIO]
-    games: List[int]
+    fens: List[str]
 
 app = typer.Typer()
-app_options = AppOptions("", None, [])
+app_options = AppOptions("", [])
 
 def get_random_fen() -> str:
-    """ Generates a random fen """
-    game = random.choice(app_options.games)
-    board = game.board()
-    for move in game.mainline_moves():
-        if random.choice([False, True]):
-            break
-        board.push(move)
-    return game.board().fen()
+    """ Returns a random FEN from the --fen-file """
+    return random.choice(app_options.fens)
 
 
 def analyse_board(fen: str) -> Optional[str]:
@@ -38,16 +31,16 @@ def analyse_board(fen: str) -> Optional[str]:
 
 
 @app.command()
-def throughput(timeout: int = typer.Argument(120, help="Time to run for in seconds")):
+def throughput(timeout: int = typer.Option(120, help="Time to run for in seconds")):
     """ Send sequental requests as quickly as possible """
     start_time = datetime.now()
     current_time = start_time
 
     while current_time - start_time <= timedelta(seconds=timeout):
-        typer.echo(get_random_fen())
+        analyse_board(get_random_fen().strip())
         current_time = datetime.now()
 
-    typer.echo(f"time elappsed: {current_time - start_time}")
+    typer.echo(f"Time elappsed: {current_time - start_time}")
 
 
 @app.command()
@@ -72,18 +65,15 @@ def convert_pgn(pgn_file: str, output_file: str):
 
 
 @app.callback()
-def main(url: str = typer.Option("localhost", help="URL of the ingress")):
+def main(url: str = typer.Option("localhost", help="URL of the ingress"),
+         fen_file: str = typer.Option("lichess_jan_2013.fens", help="File containing FENs")):
     app_options.url = url
 
-    with open("./lichess_db_standard_rated_2013-01.pgn") as pgn:
-        offset = pgn.tell()
-        game = chess.pgn.read_headers(pgn)
-        while game is not None:
-            app_options.games.append(offset)
-            game = chess.pgn.read_headers(pgn)
+    with open(fen_file) as file:
+        app_options.fens = file.readlines()
 
-    if len(app_options.games) == 0:
-        typer.echo("Could not load games", err=True)
+    if len(app_options.fens) == 0:
+        typer.echo("Could not load fens", err=True)
         typer.Exit()
 
 
