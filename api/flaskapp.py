d14d3579a8b96b8bd0a356e1ff55b36b5dc94471
danjones1618 <hello@danjones.dev>
2021-11-26 03:33:40 +0000

    Migrate to Quart

--- api/flaskapp.py
+++ api/flaskapp.py
@@ -7,25 +7,25 @@ import logging
 import uuid
 import sys
 
-from flask import abort, Flask, request
-from flask_cors import CORS
+from quart import abort, Quart, request
+from quart_cors import cors
 from pymongo import MongoClient
 import redis
 
 logging.basicConfig(stream=sys.stdout)
 logger = logging.getLogger("app")
 
-app = Flask(__name__)
-CORS(app)
+app = Quart(__name__)
+cors(app)
 
 redis_con = redis.Redis(host='redis',port=6379)
-DB_NAME = "work" # database name
-client = MongoClient('mongo',27017)
-db = client['game']
+ANALYSIS_QUEUE = "fen_analysis"
+mongo_client = MongoClient('mongo',27017)
+game_db = mongo_client['game']
 
 
 @app.route('/json-post', methods=['POST'])
-def post():
+async def post():
     """
     - send a json to the backend
     - the json contains the fen
@@ -33,18 +33,17 @@ def post():
     - store both in the mongodb with a status pending
     - push into the queue
     """
-    request_data = request.get_json() # get json
+    request_data = await request.get_json() # get json
 
     if request_data is None:
         logging.warning("Recieved an invalid request to /json-post")
         abort(400)
 
-    print(request_data)
     if 'fen' not in request_data:
         logging.warning("Recieved a request without valid properites")
         abort(400)
 
-    fen = request_data['fen'] # get fen from json
+    fen: str = request_data['fen'] # get fen from json
     logging.info("Client requested %s to be processed", fen)
 
     fen_uuid = str(uuid.uuid4()) # generate uuid
@@ -52,8 +51,10 @@ def post():
     queuetime = datetime.datetime.utcnow()
     # Item to post to mongoDB with status of pending by default
     # The time the item was queued is stored, which can be queried against as it is in the format MongoDB expects
-    fen = {"_id": fen_uuid, "fen": fen, "status": "pending", "lastqueued": queuetime}
+    fen_item = {"_id": fen_uuid, "fen": fen, "status": "pending", "lastqueued": queuetime}
 
-    db.fens.insert_one(fen) # post the item into the database
-    redis_con.rpush(DB_NAME, fen_uuid) # push the item into the redis queue
-    return "{ree:'ree'}"
+    game_db.fens.insert_one(fen_item) # post the item into the database
+    redis_con.rpush(ANALYSIS_QUEUE, fen_uuid) # push the item into the redis queue
+
+    # TODO wait for compuation to be completed and return it
+    return "{ree: 'ree'}"
