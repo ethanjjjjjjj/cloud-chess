5be719cb0c4e12eef564be954e2d66c0bcf2b636
danjones1618 <hello@danjones.dev>
2021-11-26 03:14:11 +0000

    Flake8ify

--- api/flaskapp.py
+++ api/flaskapp.py
@@ -1,50 +1,59 @@
-from flask import Flask,jsonify,Response
-from flask import request
-from flask_cors import CORS
-import redis
-import json
+"""
+Flask App providing a HTTP and WS access to the backend
+"""
+
+import datetime
 import logging
 import uuid
-import os
-import pymongo 
+import sys
+
+from flask import abort, Flask, request
+from flask_cors import CORS
 from pymongo import MongoClient
-import datetime
+import redis
+
+logging.basicConfig(stream=sys.stdout)
+logger = logging.getLogger("app")
 
 app=Flask(__name__)
 CORS(app)
-r=redis.Redis(host='localhost',port=6379)
+
+r=redis.Redis(host='localhost', port=6379)
 DB_NAME = "work" # database name
-client = MongoClient('localhost',27017)
+client = MongoClient('localhost', 27017)
 db = client['game']
 
-"""""
-- same as post but less complex? for short messages?
-- thing has to be in the url
-"""""
-# @app.route("/GET")
-# def get():
-#     el = r.lpop(DB_NAME)
-#     pass
 
-"""
-- send a json to the backend
-- the json contains the fen
-- recieve fen, take out of json into string, generate uuid
-- store both in the mongodb with a status pending
-- push into the queue
-"""
-#@app.route("/POST") # ?
 @app.route('/json-post', methods=['POST'])
 def post():
+    """
+    - send a json to the backend
+    - the json contains the fen
+    - recieve fen, take out of json into string, generate uuid
+    - store both in the mongodb with a status pending
+    - push into the queue
+    """
     request_data = request.get_json() # get json
+
+    if request_data is None:
+        logging.warning("Recieved an invalid request to /json-post")
+        abort(400)
+
     print(request_data)
-    FEN = request_data['fen'] # get fen from json
-    print(FEN)
-    UUID = uuid.uuid4().__str__() # generate uuid
-    #fen = {"_id" : UUID, "fen" : FEN, "status" : "pending", "timestamp" : datetime.datetime.utcnow()} # create the item to post in the mongoDB database, with a status of pending by default
+    if 'fen' not in request_data:
+        logging.warning("Recieved a request without valid properites")
+        abort(400)
+
+    fen = request_data['fen'] # get fen from json
+    logging.info("Client requested %s to be processed", fen)
+
+    fen_uuid = str(uuid.uuid4()) # generate uuid
+
     queuetime = datetime.datetime.utcnow()
-    fen = {"_id" : UUID, "fen" : FEN, "status" : "pending", "lastqueued" : queuetime}   # create the item to post in the mongoDB database, with a status of pending by default
-                                                                                        # The time the item was queued is stored, which can be queried against as it is in the format MongoDB expects
+    # Item to post to mongoDB with status of pending by default
+    # The time the item was queued is stored, which can be queried against as it is in the format MongoDB expects
+    fen = {"_id": fen_uuid, "fen": fen, "status": "pending", "lastqueued": queuetime}
+
     db.fens.insert_one(fen) # post the item into the database
-    r.rpush(DB_NAME, UUID) # push the item into the redis queue
+    r.rpush(DB_NAME, fen_uuid) # push the item into the redis queue
     return "{ree:'ree'}"
