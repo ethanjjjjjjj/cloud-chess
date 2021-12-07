cef7d5e9d33e2ad824afcf566d1a8c25aabc53a5
Ethan Williams <sk18181@bristol.ac.uk>
2021-12-07 10:22:23 +0000

    added autoscaling to script

--- scripts/apply-all-manifests.sh
+++ scripts/apply-all-manifests.sh
@@ -2,3 +2,7 @@ for i in $(ls kube-manifests/deploy*)
 do
 kubectl apply -f $i
 done
+for i in bot-worker fen-worker game-worker gateway
+do
+kubectl autoscale deploy/$i --max 10 --cpu-percent=80
+done
